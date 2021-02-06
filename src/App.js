6741cb95e44991638daddad58af9cf7e5b2101f3
timvansteenbergen <G-5z!NkK%U9MGDt!dNUK4S50v:d/Ce09)R0wUaD{mk=GDwkm~P+_@:v!Vz/U9mpfGH=n.##A>
2021-02-06 18:40:50 +0100

    Added GraphQL for a note: id, name, description and image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
@@ -70,27 +70,5 @@ class App extends React.Component {
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <AmplifySignOut />
-//       </header>
-//     </div>
-//   );
-// }
 
 export default withAuthenticator(App);
